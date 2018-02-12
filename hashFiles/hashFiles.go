b6505906fe67c9928c6da2e7ad4057eb4d3938c6
Wilyarti Howard <wilyarti@gmail.com>
2018-02-12 22:47:28 +0930

    Add golang version - work in progress
    	new file:   hashFiles/hashFiles.go
    	new file:   main.go
    	new file:   uploadFiles/uploadFiles.go
    	new file:   writeDB/writeDB.go

---
+++ hashFiles/hashFiles.go
@@ -0,0 +1,41 @@
+package hashFiles
+
+import (
+	"crypto/sha256"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var files = make(map[string][sha256.Size]byte)
+
+func Hash (path string, info os.FileInfo, err error) error {
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	if info.IsDir() {
+		return nil
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	digest := sha256.Sum256(data)
+	files[path] = digest
+
+	return nil
+}
+
+func Scan (path string) map[string][sha256.Size]byte {
+    dir := path
+	err := filepath.Walk(dir, Hash)
+    if err != nil {
+		log.Fatal(err)
+	}
+	return files
+}
+
